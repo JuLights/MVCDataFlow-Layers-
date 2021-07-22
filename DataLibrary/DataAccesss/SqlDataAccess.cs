28d27cc3bd18975025e4069a355699067ea4836e
jugeljke <jugeljke@gmail.com>
2021-07-22 20:06:34 +0400

    Add project files.

---
+++ DataLibrary/DataAccesss/SqlDataAccess.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataLibrary.DataAccesss
+{
+    public static class SqlDataAccess
+    {
+        //getting connection string
+        //connectionName is in webconfig(<connectionString><connectionString>)
+        public static string GetConnectionString(string connectionName = "MVCDemoDB")
+        {
+            return ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
+        }
+
+        //loading data from db
+        public static List<T> LoadData<T>(string sql)
+        {
+            using (IDbConnection cnn = new SqlConnection(GetConnectionString()))
+            {
+                return cnn.Query<T>(sql).ToList();
+            }
+        }
+
+        //saving data in db
+        public static int SaveData<T>(string sql,T data)
+        {
+            using (IDbConnection cnn = new SqlConnection(GetConnectionString()))
+            {
+                return cnn.Execute(sql,data);
+            }
+        }
+
+    }
+}
